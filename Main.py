36eb4e54d6c4fd4161fcbf1339c0a0a3da4b6dd9
Vladislav Zhigalov <36502032+Vladqwelkj@users.noreply.github.com>
2019-01-01 18:17:08 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,36 +1,41 @@
 import json, requests
-address1 = "Write the correctx address( t1UGKeunkVVUBMfjEM9USEoapLKJMu7nchC ):\n"  #прием кошелька от пользователя(поменял для регулярного пользования. Надо прописать input()
-address = 't1UGKeunkVVUBMfjEM9USEoapLKJMu7nchC' # для регулярного пользования сменил на статичный кошелек
+
+__address__ = input("Write the correctx address:\n")
 
 def CheckCorrectWallet(address):
+    global __address__
     while True:                                         #проверка корректности кошелька
-        if len(address) == 35 and list(address)[0] == 'z':  #проверка на анонимность кошелька
+        if len(__address__) == 35 and list(__address__)[0] == 'z':  #проверка на анонимность кошелька
             print('ERROR: Sorry, this wallet is anonymous. Unable to view balance')
-            address = input("Write the address(not anonymous):\n")   # сброс значения адреса и предложение ввести другой
+            __address__ = input("Write the address(not anonymous):\n")   # сброс значения адреса и предложение ввести другой
             continue
-        elif len(address) == 35 and list(address)[0] == 't':  # если кошелек корректный, продолжить
-            site = "https://api.zcha.in/v2/mainnet/accounts/" + address
+        elif len(__address__) == 35 and list(__address__)[0] == 't':  # если кошелек корректный, продолжить
+            site = "https://api.zcha.in/v2/mainnet/accounts/" + __address__
             return site
             break
         else:
             print('ERROR: Sorry, you entered an incorrect address.')
-            address = input("Write the correct address:\n") # сброс значения адреса и предложение ввести другой
+            __address__ = input("Write the correct address:\n") # сброс значения адреса и предложение ввести другой
             continue
         
-def BalanceZec():           #получение баланса
-    getApi = requests.get(CheckCorrectWallet(address))
+        
+def BalanceZec():           #getting balance info
+    getApi = requests.get(CheckCorrectWallet(__address__))
     data = json.loads(getApi.text)
     return data['balance']
 
-def RateRUB_ZEC():   #получение курса ZEC-RUB
+
+def RateRUB_ZEC():   #rate ZEC-RUB
     getApi = requests.get('https://api.coinmarketcap.com/v2/ticker/1437/?convert=RUB')
     data = json.loads(getApi.text)
     return data["data"]["quotes"]['RUB']["price"]
 
-def RateUSD_ZEC():  #получение курса ZEC-USD
+
+def RateUSD_ZEC():  #rate ZEC-USD
     getApi = requests.get('https://api.coinmarketcap.com/v2/ticker/1437/?convert=RUB')
     data = json.loads(getApi.text)
     return data["data"]["quotes"]['USD']["price"]
+
 
 print('ZEC balance: ' + str(BalanceZec()))
 print('ZEC balance in USD: ' + str(RateUSD_ZEC()*BalanceZec()))
